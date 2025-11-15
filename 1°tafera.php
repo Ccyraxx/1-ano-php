9dd6b81f5f703218c12292701c8fee2821c56c00
Ccyraxx <carloseduardoaguero01@gmail.com>
2025-11-14 22:43:50 -0300

    Update 1°tafera.php

--- 1°tafera.php
+++ 1°tafera.php
@@ -1,28 +1,20 @@
 <?php
 
-    $erros;
-    $certo;
+    $erros = 0;//Valor da variável!
 
-    echo 'Você terar que acerta um número de 1 até 10 só vai poder errar 5 vezes';
+    echo "Você terar que acerta um número de 1 até 10 só vai poder errar 5 vezes\n";//Instruçoes do programa!
 
-    while($erros <= 5){
-    
-        $sorteio = rand( 1,10);
-
-        $num = readline ('Informe o número!'.'\n');
-                if($num == $sorteio){
-                    echo 'voce acertou\n';
-                    $certo = $certo + 1;
-                    if($certo = 1){
-                        echo 'Você acertou!\n';
-                        break;
-                    }
-                }else{
-                    echo 'Você errou!\n';
-                    $erros = $erros + 1;
-                    if($erros == 6){
-                        echo '\n.'.'Nossa que ruim!kkk';
-                        break;
-                    }
-                }
-    }
+    while($erros <= 5){//Vendo se a variàvel $erros igual-menor que 5!
+        $sorteio = rand( 1,10);//Sorteia um número de 1 ate 10!
+        $numerosorteado = readline ("Informe o número!\n");//Pergunta qual números você acha que è!
+            if($numerosorteado == $sorteio){//Vendo se o números que você informou e igual ao números sorteado!
+                echo "voce acertou\n";//Escreve que você acertou o números sorteado!
+            }else{//Caso você tenha errado!
+                echo "Você errou!\n";//Escreve que você errou o números sorteado
+                $erros++;//Acrecentar um a maìs!
+                if($erros == 5){//Vendo se a variàvel $erros è igual a 5!
+                    echo "Nossa que ruim!kkk";//Escreve que você errou as 5 chanches!
+                    break;//Parar tudo!
+                }//Fim do if
+            }//Fim do senão
+    }//Fim do while
